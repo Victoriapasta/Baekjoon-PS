d886bd40e88847aa12045c845a3230bcb2e731a2
Victoriapasta <103020630+Victoriapasta@users.noreply.github.com>
2023-06-30 18:12:19 +0900

    [Silver V] Title: 타일 장식물, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 4;
+
+        if(n > 1) {
+            dp[2] = 6;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
