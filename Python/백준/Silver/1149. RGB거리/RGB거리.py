f821a93a2308442cc51902046df4b599c0a4753c
Victoriapasta <103020630+Victoriapasta@users.noreply.github.com>
2024-03-30 15:44:53 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+cost = []
+
+# 0번 : 빨강
+# 1번 : 초록
+# 2번 : 파랑
+for _ in range(n):
+    cost.append(list(map(int, input().split())))
+
+dp = [[0, 0, 0] for _ in range(n)]
+dp[0][0] = cost[0][0]
+dp[0][1] = cost[0][1]
+dp[0][2] = cost[0][2]
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i - 1][1] + cost[i][0], dp[i - 1][2] + cost[i][0])
+    dp[i][1] = min(dp[i - 1][0] + cost[i][1], dp[i - 1][2] + cost[i][1])
+    dp[i][2] = min(dp[i - 1][0] + cost[i][2], dp[i - 1][1] + cost[i][2])
+
+print(min(dp[n-1]))
