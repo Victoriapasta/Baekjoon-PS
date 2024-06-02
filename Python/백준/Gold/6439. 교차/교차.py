e6ab3bfbf002e02f72324b4cefbf9539f5cda4dc
Victoriapasta <103020630+Victoriapasta@users.noreply.github.com>
2024-06-02 16:24:33 +0900

    [Gold I] Title: 교차, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/6439. 교차/교차.py
+++ Python/백준/Gold/6439. 교차/교차.py
@@ -14,12 +14,14 @@ def findCross(x1, y1, x2, y2, x3, y3, x4, y4):
     d = ccw(x3, y3, x4, y4, x2, y2)
 
     if a * b <= 0 and c * d <= 0:
-        if x1 < x3 and x1 < x4 and x2 < x3 and x2 < x4 \
-                or x3 < x1 and x3 < x2 and x4 < x1 and x4 < x2:
+        if x1 < x3 and x1 < x4 and x2 < x3 and x2 < x4:
+            return False
+        if x3 < x1 and x3 < x2 and x4 < x1 and x4 < x2:
             return False
 
-        if y1 < y3 and y1 < y4 and y2 < y3 and y2 < y4 \
-                or y3 < y1 and y3 < y2 and y4 < y1 and y4 < y2:
+        if y1 < y3 and y1 < y4 and y2 < y3 and y2 < y4:
+            return False
+        if y3 < y1 and y3 < y2 and y4 < y1 and y4 < y2:
             return False
 
         return True
